b0065611a2f098bf5abfab8255acce447c823725
arifineu <arifin.izzah15@gmail.com>
2023-12-31 15:08:11 +0700

    feat: integrate persona

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@privyid/persona',
+    '@privyid/pong'
+  ],
+  css    : ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      'postcss-hexrgba'       : {},
+      'tailwindcss/nesting'   : {},
+      'tailwindcss'           : {},
+      'postcss-lighten-darken': {},
+      'autoprefixer'          : {},
+    },
+  },
 })
